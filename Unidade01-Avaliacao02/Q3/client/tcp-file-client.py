78d7320b10d8bbe74270ea068a04e445582a49d9
eduardobezerraz <dudubezerr4@gmail.com>
2025-01-05 13:04:27 -0300

    Q3: adiconadas funcionalidades sget e mget

--- Unidade01-Avaliacao02/Q3/client/tcp-file-client.py
+++ Unidade01-Avaliacao02/Q3/client/tcp-file-client.py
@@ -1,5 +1,6 @@
 import socket
 import hashlib 
+import os
 
 DIRBASE = "files/"
 SERVER = '127.0.0.1'
@@ -9,36 +10,95 @@ socktcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socktcp.connect((SERVER, PORT))
 
 while True:
-    comando = input("Digite 'list' para listar arquivos ou o nome de um arquivo para baixá-lo: ").strip()
+    print("\nLista de possíveis comandos:")
+    print("A) list  -Lista arquivos presentes no servidor.")
+    print("B) sget <nome_do_arquivo> -Solicita o download de um arquivo do servidor.")
+    print("C) mget <máscara> - Solicitar download de múltiplos arquivos.")
+    print("D) exit -Encerra a conexão e o programa.")
     
-    if comando == "list":
-        #3 a) Solicitar listagem de arquivos ao servidor
-        socktcp.send(comando.encode('utf-8'))
-        resposta = socktcp.recv(4096).decode('utf-8')
-        print("Arquivos disponíveis:")
-        print(resposta)
-    else:
-        # Solicitar um arquivo específico
-        socktcp.send(comando.encode('utf-8'))
+    comandoValido = False
+    comando = input("\ncomando: ").strip()
+    
+    while not comandoValido:
+        if comando == "list":
+            #3 a) Solicitar listagem de arquivos ao servidor
+            socktcp.send(comando.encode('utf-8'))
+            resposta = socktcp.recv(4096).decode('utf-8')
+            print("Arquivos disponíveis:")
+            print(resposta)
 
-        try:
-            tamanho_arquivo = int.from_bytes(tamanho_arquivo, 'big')
+            comandoValido = True
+        elif "sget" in comando:
+            # Solicitar um arquivo específico
+            socktcp.send(comando.encode('utf-8'))
+
+            # Recebe o tamanho do arquivo em 8 bytess
+            resposta = socktcp.recv(8)
+            tamanho_arquivo = int.from_bytes(resposta, 'big')
             print(f"Tamanho do arquivo: {tamanho_arquivo} bytes")
 
+            # Separa o sget e o nome do arquivo
+            comando, fileName = comando.split(maxsplit=1)
+
             # Grava o arquivo localmente
-            print(f"Gravando arquivo localmente: {comando}")
-            with open(DIRBASE + comando, 'wb') as file:
+            print(f"Gravando arquivo localmente: {fileName}")
+            localFilePath = os.path.join(DIRBASE, fileName)
+            with open( localFilePath , 'wb') as file:
                 recebido = 0
+                count = 100
                 while recebido < tamanho_arquivo:
                     data, source = socktcp.recvfrom(4096)
                     file.write(data)
                     recebido += len(data)
-                print(f"Arquivo {comando} recebido com sucesso!")
-        except ValueError:
-            # Trata mensagem de erro do servidor
-            resposta = socktcp.recv(4096).decode('utf-8')
-            print(f"Erro recebido do servidor: {resposta}")
+                    if count % 100 == 0:
+                        print(f"Recebido {recebido}/{tamanho_arquivo} bytes...")
+                    count += 1
+                print(f"Arquivo recebido com sucesso!\n")
 
-    # Fecha o socket ao final
-    socktcp.close()
-    exit()
+            comandoValido = True
+        elif "mget" in comando:
+            try:
+                socktcp.send(comando.encode('utf-8'))
+                resposta = socktcp.recv(4096).decode('utf-8')
+                
+                if "Erro" in resposta:
+                    print(resposta)
+                else:
+                    num_arquivos = int(resposta)
+                    print(f"Número de arquivos encontrados: {num_arquivos}")
+
+                    for aux in range(num_arquivos):
+                        fileName = socktcp.recv(512).decode('utf-8')
+                        socktcp.send(b'1')
+                        fileSize = int.from_bytes(socktcp.recv(8), 'big')
+                        socktcp.send(b'1')
+
+                        localFilePath = os.path.join(DIRBASE, fileName)
+
+                        print(f"Recendo arquivo: {fileName} com {fileSize} bytes.\n")
+                        with (open(localFilePath, 'wb') as file):
+                            recebido = 0
+                            count = 100
+                            while recebido < fileSize:
+                                chunk = socktcp.recv(4096)
+                                file.write(chunk)
+                                recebido += len(chunk)
+                                if count % 100 == 0:
+                                    print(f"Recebidos {recebido} de {tamanho_arquivo} do arquivo {fileName}.")
+                                count += 1
+                            print(f"\nArquivo {fileName} salvo em {localFilePath}.")
+                        
+                        
+
+            except Exception as e:
+                print(f"Erro no comando 'mget': {str(e)}")
+
+         # Encerra a conexão e o programa   
+        elif comando == "exit":
+            print("Encerrando conexão com o servidor.")
+            socktcp.send(comando.encode('utf-8'))
+            socktcp.close()
+            exit()
+        else:
+            print("Comando inválido, tente novamente.")
+            comando = input("\ncomando: ").strip()
