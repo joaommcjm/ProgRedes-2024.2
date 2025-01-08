c23eb1b069c9ce34c81963b4dfc3394ffc65a293
eduardobezerraz <dudubezerr4@gmail.com>
2025-01-07 23:55:59 -0300

    Versão final Q3 + pdf

--- Unidade01-Avaliacao02/Q3/client/tcp-file-client.py
+++ Unidade01-Avaliacao02/Q3/client/tcp-file-client.py
@@ -2,103 +2,185 @@ import socket
 import hashlib 
 import os
 
+
 DIRBASE = "files/"
 SERVER = '127.0.0.1'
 PORT = 12345
 
+# Criação do socket TCP e conexão com o servidor
 socktcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socktcp.connect((SERVER, PORT))
 
+# Loop principal para interação com o servidor
 while True:
+    # Exibe a lista de comandos disponíveis para o usuário
     print("\nLista de possíveis comandos:")
     print("A) list  -Lista arquivos presentes no servidor.")
     print("B) sget <nome_do_arquivo> -Solicita o download de um arquivo do servidor.")
-    print("C) mget <máscara> - Solicitar download de múltiplos arquivos.")
-    print("D) exit -Encerra a conexão e o programa.")
-    
+    print("C) mget <máscara> (Ex: *.jpg) - Solicitar download de múltiplos arquivos.")
+    print("D) hash <nome_do_arquivo> <posição> - Calcula o hash do arquivo até uma posição especifica")
+    print("E) cget <nome_do_arquivo> - Continua o download de um arquivo")
+    print("F) exit -Encerra a conexão e o programa.")
+
+    # Variável de controle para validar comandos
     comandoValido = False
-    comando = input("\ncomando: ").strip()
-    
+    comando = input("\ncomando: ")
+
+    # Loop para garantir que o comando seja válido
     while not comandoValido:
-        if comando == "list":
-            #3 a) Solicitar listagem de arquivos ao servidor
-            socktcp.send(comando.encode('utf-8'))
-            resposta = socktcp.recv(4096).decode('utf-8')
-            print("Arquivos disponíveis:")
-            print(resposta)
+        try:
+             # Comando para listar arquivos no servidor
+            if comando == "list":
+                socktcp.send(comando.encode('utf-8')) # Envia o comando ao servidor
+                resposta = socktcp.recv(4096).decode('utf-8') # Recebe a resposta do servidor
+                print("Arquivos disponíveis:")
+                print(resposta)
 
-            comandoValido = True
-        elif "sget" in comando:
-            # Solicitar um arquivo específico
-            socktcp.send(comando.encode('utf-8'))
+                comandoValido = True
 
-            # Recebe o tamanho do arquivo em 8 bytess
-            resposta = socktcp.recv(8)
-            tamanho_arquivo = int.from_bytes(resposta, 'big')
-            print(f"Tamanho do arquivo: {tamanho_arquivo} bytes")
-
-            # Separa o sget e o nome do arquivo
-            comando, fileName = comando.split(maxsplit=1)
-
-            # Grava o arquivo localmente
-            print(f"Gravando arquivo localmente: {fileName}")
-            localFilePath = os.path.join(DIRBASE, fileName)
-            with open( localFilePath , 'wb') as file:
-                recebido = 0
-                count = 100
-                while recebido < tamanho_arquivo:
-                    data, source = socktcp.recvfrom(4096)
-                    file.write(data)
-                    recebido += len(data)
-                    if count % 100 == 0:
-                        print(f"Recebido {recebido}/{tamanho_arquivo} bytes...")
-                    count += 1
-                print(f"Arquivo recebido com sucesso!\n")
-
-            comandoValido = True
-        elif "mget" in comando:
-            try:
+            # Comando para baixar um único arquivo    
+            elif "sget" in comando:
+                # Solicita ao servidor um arquivo específico 
                 socktcp.send(comando.encode('utf-8'))
-                resposta = socktcp.recv(4096).decode('utf-8')
+                try:
+                    # Recebe o tamanho do arquivo em 8 bytes
+                    resposta = socktcp.recv(8)
+                    tamanho_arquivo = int.from_bytes(resposta, 'big')
+                    print(f"Tamanho do arquivo: {tamanho_arquivo} bytes")
+
+                    # Separa o sget e o nome do arquivo
+                    comando, fileName = comando.split(maxsplit=1)
+
+                    # Grava o arquivo localmente
+                    print(f"Gravando arquivo localmente: {fileName}")
+                    localFilePath = os.path.join(DIRBASE, fileName)
+                    with open( localFilePath , 'wb') as file:
+                        recebido = 0
+                        count = 100         # Variável criada para printar bytes recebidos a cada 100 repetições do laço
+                        
+                        # Recebe o tamanho do arquivo em 8 bytes
+                        while recebido < tamanho_arquivo:
+                            data, source = socktcp.recvfrom(4096)
+                            file.write(data)
+                            recebido += len(data)
+                            if count % 100 == 0:
+                                print(f"Recebido {recebido}/{tamanho_arquivo} bytes...")
+                            count += 1
+                        print(f"Arquivo recebido com sucesso!\n")
+
+                except Exception as e:
+                    #Exibe erros que podem ocorrer durante o recebimento
+                    print(f"Erro ao receber o arquivo: {str(e)}")
+                comandoValido = True
+
+            # Comando para baixar múltiplos arquivos    
+            elif "mget" in comando:
+                try:
+                    # Envia o comando para obter vários arquivos
+                    socktcp.send(comando.encode('utf-8'))
+                    resposta = socktcp.recv(4096).decode('utf-8')
                 
-                if "Erro" in resposta:
-                    print(resposta)
-                else:
-                    num_arquivos = int(resposta)
-                    print(f"Número de arquivos encontrados: {num_arquivos}")
+                    if "Erro" in resposta:
+                        print(resposta) # Exibe mensagem de erro do servidor
+                        break
+                    else:
+                        # Número de arquivos que atenderam à máscara especificada
+                        num_arquivos = int(resposta)
+                        print(f"Número de arquivos encontrados: {num_arquivos}")
+                    
+                        for aux in range(num_arquivos):
+                            try:
+                                # Recebe o nome do arquivo 
+                                fileName = socktcp.recv(512).decode('utf-8')
+                                socktcp.send(b'1') # Confirmação para o servidor
+                                fileSize = int.from_bytes(socktcp.recv(8), 'big')
+                                socktcp.send(b'1') # Confirmação para o servidor
 
-                    for aux in range(num_arquivos):
-                        fileName = socktcp.recv(512).decode('utf-8')
-                        socktcp.send(b'1')
-                        fileSize = int.from_bytes(socktcp.recv(8), 'big')
-                        socktcp.send(b'1')
+                                # Cria o arquivo localmente para gravar os dados recebidos
+                                localFilePath = os.path.join(DIRBASE, fileName)
 
-                        localFilePath = os.path.join(DIRBASE, fileName)
+                                print(f"Recendo arquivo: {fileName} com {fileSize} bytes.\n")
+                                with (open(localFilePath, 'wb') as file):
+                                    recebido = 0
+                                    count = 100         # Variável criada para printar bytes recebidos a cada 100 repetições do laço
 
-                        print(f"Recendo arquivo: {fileName} com {fileSize} bytes.\n")
-                        with (open(localFilePath, 'wb') as file):
-                            recebido = 0
-                            count = 100
-                            while recebido < fileSize:
-                                chunk = socktcp.recv(4096)
-                                file.write(chunk)
-                                recebido += len(chunk)
-                                if count % 100 == 0:
-                                    print(f"Recebidos {recebido} de {tamanho_arquivo} do arquivo {fileName}.")
-                                count += 1
-                            print(f"\nArquivo {fileName} salvo em {localFilePath}.")
-                        
-                        
+                                     # Recebe e grava o arquivo em partes
+                                    while recebido < fileSize:
+                                        chunk = socktcp.recv(4096)
+                                        file.write(chunk)
+                                        recebido += len(chunk)
+                                        if count % 100 == 0:
+                                            print(f"Recebidos {recebido} de {fileSize} do arquivo {fileName}.")
+                                        count += 1
+                                    print(f"\nArquivo {fileName} salvo em {localFilePath}.")
+                            except Exception as e:
+                                # Exibe erro que ocorrem durante o download de cada arquivo
+                                print(f"Erro ao processar o arquivo {fileName}: {str(e)}")
+                            comandoValido = True
+            
+                except Exception as e:
+                    # Exibe erro relacionado ao comando 'mget'
+                    print(f"Erro no comando 'mget': {str(e)}")         
+            
+            # Comando para calcular o hash até uma posição específica
+            elif "hash" in comando:
+                try:
+                    socktcp.send(comando.encode('utf-8')) # Envia o comando ao servidor
+                    resposta = socktcp.recv(4096).decode('utf-8')  # Recebe o hash calculado pelo servidor
+                    print(f"Hash recebido do servidor: {resposta}")
+                except Exception as e:
+                    print(f"Erro ao solicitar o hash: {str(e)}")
+                comandoValido = True
+            
+            # Comando para continuar o download de um arquivo
+            elif "cget" in comando:
+                try:
+                    socktcp.send(comando.encode('utf-8'))
+                    comando, fileName = comando.split(maxsplit=1)
+                    localFilePath = os.path.join(DIRBASE, fileName)
 
-            except Exception as e:
-                print(f"Erro no comando 'mget': {str(e)}")
+                    if not os.path.exists(localFilePath):
+                        print("Arquivo não encontrado localmente para continuar download.")
+                    else:
+                        # Calcula o tamanho e o hash da parte existente
+                        local_size = os.path.getsize(localFilePath)
+                        with open(localFilePath, 'rb') as f:
+                            local_data = f.read()
+                            local_hash = hashlib.sha1(local_data).hexdigest()
 
-         # Encerra a conexão e o programa   
-        elif comando == "exit":
-            print("Encerrando conexão com o servidor.")
-            socktcp.send(comando.encode('utf-8'))
-            socktcp.close()
-            exit()
-        else:
-            print("Comando inválido, tente novamente.")
-            comando = input("\ncomando: ").strip()
+                        # Envia tamanho e hash local para o servidor
+                        socktcp.send(local_size.to_bytes(8, 'big'))
+                        socktcp.send(local_hash.encode('utf-8'))
+
+                        # Recebe resposta do servidor sobre a continuidade
+                        server_response = socktcp.recv(512).decode('utf-8')
+                        if "Erro" in server_response:
+                            print(server_response)
+                        else:
+                            # Continua o download a partir do tamanho local
+                            fileSize = int(server_response)
+                            with open(localFilePath, 'ab') as file:
+                                recebido = local_size
+                                while recebido < fileSize:
+                                    chunk = socktcp.recv(4096)
+                                    file.write(chunk)
+                                    recebido += len(chunk)
+                                print(f"Download concluído: {fileName}")
+                    comandoValido = True
+                except Exception as e:
+                    print(f"Erro! Não foi possível continuar o download do arquivo: {str(e)}")
+                    comandoValido = True
+
+            # Encerra a conexão e o programa   
+            elif comando == "exit":
+                    print("Encerrando conexão com o servidor.")
+                    socktcp.send(comando.encode('utf-8'))
+                    socktcp.close()
+                    exit()
+            else:
+                print("Comando inválido, tente novamente.")
+                comando = input("\ncomando: ").strip()
+        except Exception as e:
+            # Exibe qualquer erro que ocorra durante o processamento de comandos
+            print(f"Erro no processamento do comando: {str(e)}")
+            
--- Unidade01-Avaliacao02/Q3/server/tcp-file-server.py
+++ Unidade01-Avaliacao02/Q3/server/tcp-file-server.py
@@ -1,5 +1,4 @@
 import socket
-import struct
 import os
 import hashlib
 import glob
@@ -9,17 +8,21 @@ DIRBASE = "files/"
 INTERFACE = '127.0.0.1'
 PORT = 12345
 
+# Criação do socket TCP
 socktcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-socktcp.bind((INTERFACE, PORT))
-socktcp.listen(1)
+socktcp.bind((INTERFACE, PORT)) # Associa o socket ao endereço e porta especificados
+socktcp.listen(1) # O servidor começa a escutar conexões
 
 print("Escutando em ...", (INTERFACE, PORT))
 
+# Laço principal para aceitar conexões de clientes
 while True:
     try:
         # Recebe a conexão
         conn, addr = socktcp.accept() 
         print(f"Conexão recebida de: {addr}")
+        
+        # Laço de processamento dos comandos
         while True:
             print("\n Aguardando comando...\n")
             # Recebe o comando do cliente
@@ -29,6 +32,7 @@ while True:
             # Lista os arquivos presentes no servidor
             if comando == "list":
                 try:
+                    # Obtém a lista de arquivos no diretório base
                     serverFiles = os.listdir(DIRBASE)
                     finalFiles = ''
                     for file in serverFiles:
@@ -36,38 +40,45 @@ while True:
                         if os.path.isfile(filePath):
                             fileSize = os.path.getsize(filePath)
                             finalFiles += "{:<20} {:>15} bytes.\n".format(file, fileSize)
-
+            
                     # Envia a listagem de arquivos para o cliente
                     print("Enviando lista de arquivos...")
                     print(finalFiles)
                     conn.send(finalFiles.encode())
-
+                
+                # Trata erros ao listar os arquivos
                 except Exception as e:
                     erro = f"Erro ao listar arquivos: {str(e)}"
                     print(erro)
                     conn.send(erro.encode('utf-8'))
 
-            # Envia o arquivo solicitado        
+            # Envia o arquivo solicitado pelo cliente   
             elif "sget" in comando:
                 try:
                     # Separa os argumentos do comando
                     comando, fileName = comando.split(maxsplit=1)
                     filePath = os.path.join(DIRBASE, fileName)
+                    
+                    # Garante que o caminho está dentro de DIRBASE
+                    if not os.path.realpath(filePath).startswith(os.path.realpath(DIRBASE)):
+                        erro = f"Erro: Acesso ao arquivo '{fileName}' negado."
+                        print(erro)
+                        conn.send(erro.encode('utf-8'))
 
                     # Verifica se o arquivo existe
-                    if not os.path.isfile(filePath):
+                    elif not os.path.isfile(filePath):
                         erro = f"Erro: Arquivo '{fileName}' não encontrado."
                         print(erro)
                         conn.send(erro.encode('utf-8')) # Envia a mensagem de erro para o cliente.
-
+                    
                     # Processa o arquivo caso ele exista
                     else:
-                        with open(DIRBASE + fileName, 'rb') as file:
-                            file.seek(0, 2)                                 # Percorre o arquivo do inicio ao fim
-                            tamanho_arquivo = file.tell()                   # Pega o tamanho do arquivo
+                        with open(filePath, 'rb') as file:
+                            file.seek(0, 2) # Percorre o arquivo do inicio ao fim
+                            tamanho_arquivo = file.tell() # Pega o tamanho do arquivo
                             print(f"Tamanho do arquivo: {tamanho_arquivo}")
-                            file.seek(0)                                    # Volta para o início do arquivo
-                            conn.send(tamanho_arquivo.to_bytes(8, 'big'))   # Envia o tamanho do arquvio ao cliente
+                            file.seek(0) # Volta para o início do arquivo
+                            conn.send(tamanho_arquivo.to_bytes(8, 'big')) # Envia o tamanho do arquvio ao cliente
 
                             print(f"Enviando o conteúdo do arquivo: {fileName}")
                             # Lê o arquivo em blocos de 4096, := atribui o resultado à chunk e verifica se ela não está vazia.
@@ -79,44 +90,118 @@ while True:
                                 if count % 100 == 0:
                                     print(f"Enviando {bytes_enviados} de {tamanho_arquivo} do arquivo: {fileName}")
                                 count += 1
-
+                
+                # Trata exceções no comando sget
                 except Exception as e:
-                    print("Erro no comando 'sget': {e}")
-                    print(erro)
-                    conn.send(erro.encode('utf-8'))
+                    print(f"Erro no comando 'sget': {str(e)}")
+                    conn.send(f"Erro no comando 'sget': {str(e)}".encode('utf-8'))
             # Multiplas solicitações de arquivos
             elif "mget" in comando:
-                
+                try:
+                    # Divide o comando e a máscara fornecida
                     comando, mask = comando.split(maxsplit=1)
 
                     # Busca arquivos correspondentes a máscara
                     filePaths = glob.glob(os.path.join(DIRBASE, mask))
+                    safePaths = [fp for fp in filePaths if os.path.realpath(fp).startswith(os.path.realpath(DIRBASE))]
 
-                    if not filePaths:
-                        msg = f"Erro: Nenhum arquivo encontrada com a máscara."
+                    # Caso nenhum arquivo seja encontrado ou não exista caminho seguro envia uma mensagem de erro
+                    if not filePaths or not safePaths:
+                        msg = f"Erro: Máscara inválida."
                         print(msg)
                         conn.send(msg.encode('utf-8'))
                     else:
                         # Envia a quantidade de arquivos encontrados para o cliente
-                        conn.send(str(len(filePaths)).encode('utf-8'))
-                        for filePath in filePaths:
+                        conn.send(str(len(safePaths)).encode('utf-8'))
+                        for filePath in safePaths:
                             fileName = os.path.basename(filePath)
                             fileSize = os.path.getsize(filePath)
 
-                            # Envia as informações do arquivo
+                            # Envia o nome e tamanho do arquivo
                             conn.send(fileName.encode('utf-8'))
                             conn.recv(1) 
                             conn.send(fileSize.to_bytes(8,'big'))
                             conn.recv(1)
 
-                            # Envia o arquivo
+                            # Envia o conteúdo do arquivo
                             with open(filePath, 'rb') as file:
                                 print(f"Enviando arquivo {fileName} com {fileSize} bytes.")
                                 while chunk := file.read(4096):
                                     conn.send(chunk)
-                            print("Arquivo enviado.")
-                
-            # Encerra a conexão e o programa
+                            print("Arquivo enviado.")    
+
+                except Exception as e:
+                    erro = f"Erro no comando 'mget': {str(e)}\nSiga o padrão de comando: 'mget <máscara> (Ex: *.jpg)'"
+                    print(erro)
+                    conn.send(erro.encode('utf-8'))
+
+            # Implementação do cálculo de hash até uma posição específica
+            elif "hash" in comando:
+                try:
+                    # Divide o comando, nome do arquivo e posição
+                    comando, fileName, pos = comando.split(maxsplit=2)
+                    filePath = os.path.join(DIRBASE, fileName)
+
+                    # Verifica se o arquivo existe
+                    if not os.path.isfile(filePath):
+                        erro = f"Erro: Arquivo '{fileName}' não encontrado."
+                        print(erro)
+                        conn.send(erro.encode('utf-8'))
+                    else:
+                        # Lê o conteúdo do arquivo até a posição especificada
+                        pos = int(pos)
+                        with open(filePath, 'rb') as file:
+                            data = file.read(pos)
+                            hash_value = hashlib.sha1(data).hexdigest() # Calcula o hash SHA-1 do conteúdo lido
+                            conn.send(hash_value.encode('utf-8')) # Envia o valor do hash para o cliente
+                            print(f"Hash SHA-1 até a posição {pos} do arquivo {fileName}: {hash_value}")
+                except Exception as e:
+                    # Trata erros durante o cálculo do hash
+                    erro = f"Erro ao calcular o hash: {str(e)}"
+                    print(erro)
+                    conn.send(erro.encode('utf-8'))
+          
+            elif "cget" in comando:
+                try:
+                    # Separa o comando e o nome do arquivo
+                    comando, fileName = comando.split(maxsplit=1)
+                    filePath = os.path.join(DIRBASE, fileName)
+
+                    # Verifica se o arquivo existe no servidor
+                    if not os.path.isfile(filePath):
+                        erro = f"Erro: Arquivo '{fileName}' não encontrado no servidor."
+                        conn.send(erro.encode('utf-8'))
+                    else:
+                        # Recebe tamanho e hash do cliente da parte existente no cliente
+                        client_size = int.from_bytes(conn.recv(8), 'big')
+                        client_hash = conn.recv(40).decode('utf-8')
+
+                        # Calcula o hash da parte correspondente no servidor
+                        with open(filePath, 'rb') as file:
+                            server_data = file.read(client_size)
+                            server_hash = hashlib.sha1(server_data).hexdigest()
+                            
+                            #print(f"{client_hash}\n{server_hash}")
+                            # Verifica se o hash recebido do cliente coincide com o do servidor
+                            if client_hash != server_hash:
+                                erro = "Erro: Hash da parte existente não coincide com o servidor."
+                                conn.send(erro.encode('utf-8'))
+                            else:
+                                # Envia o tamanho total do arquivo
+                                fileSize = os.path.getsize(filePath)
+                                conn.send(str(fileSize).encode('utf-8'))
+                                file.seek(client_size)
+                                # Envia o restante do arquivo a partir da posição onde o cliente parou
+                                file.seek(client_size)
+                                while chunk := file.read(4096):
+                                    conn.send(chunk)
+                                    
+                # Trata erros durante o comando 'cget'
+                except Exception as e:
+                    erro = f"Erro no comando 'cget': {str(e)}"
+                    conn.send(erro.encode('utf-8'))
+
+             # Encerra a conexão e o programa
             elif "exit" in comando:
                 print("Encerrando servidor...")
                 conn.close()
@@ -128,8 +213,9 @@ while True:
                 conn.send(msg.encode('utf-8'))
     except Exception as e:
         print(f"Erro: {e}")
-        
+
     finally:
         conn.close()
         print("Conexão encerrada.")
         exit()
+        
